32c6a0168faed8d9e83942df149033f0ea23707b
MezzoDiego <diegomezzo90@gmail.com>
2023-01-02 18:25:58 +0100

    termine progetto

--- src/main/java/it/prova/pizzastore/repository/ordine/OrdineRepository.java
+++ src/main/java/it/prova/pizzastore/repository/ordine/OrdineRepository.java
@@ -22,7 +22,7 @@ public interface OrdineRepository extends CrudRepository<Ordine, Long>, CustomOr
 	@Query(value = "select count(op.pizza_id) from ordine_pizza op join ordine o on o.id = op.ordine_id where o.data between ?1 and ?2", nativeQuery = true)
 	Integer countPizzeOrderedBetween(LocalDate dataInizio, LocalDate dataFine);
 	
-	@Query("select distinct c from Ordine o join o.cliente c where c is not null and o.costoTotale > 100 and o.data between ?1 and ?2")
+	@Query("select distinct c from Ordine o join o.cliente c where c is not null and o.costoTotale > 20 and o.data between ?1 and ?2")
 	List<Cliente> findAllClientiVirtuosiBetween(LocalDate dataInizio, LocalDate dataFine);
 	
 	@Query("from Ordine o join o.fattorino f where o.closed = false and f.id =?1")
